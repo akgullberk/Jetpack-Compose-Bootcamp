0076fe5be9eb8eb7af68e275647af9029d7a4f22
Berk Akgül <berkak_gul51@hotmail.com>
2024-12-05 15:36:37 +0300

    init

---
+++ 8.gun/MVVMKullanimi/app/src/main/java/com/example/mvvmkullanimi/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.mvvmkullanimi
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mvvmkullanimi.ui.theme.MVVMKullanimiTheme
+
+class MainActivity : ComponentActivity() {
+    val anasayfaViewModel:AnasayfaViewModel by viewModels()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MVVMKullanimiTheme {
+                Anasayfa(anasayfaViewModel)
+            }
+        }
+    }
+}
+

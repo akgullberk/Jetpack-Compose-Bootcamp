b367e4da244b50a59b4f17fd73c69fab07327b00
Berk Akgül <berkak_gul51@hotmail.com>
2024-12-06 17:47:45 +0300

    init

---
+++ 11.gun/KisilerUygulamasi/app/src/main/java/com/example/kisileruygulamasi/data/entity/Kisiler.kt
@@ -0,0 +1,6 @@
+package com.example.kisileruygulamasi.data.entity
+
+data class Kisiler(var kisi_id:String? = "",
+                   var kisi_ad:String? = "",
+                   var kisi_tel:String? = "") {
+}
